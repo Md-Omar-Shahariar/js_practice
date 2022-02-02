ec6f1f8c52ef262094fdc12bd7bacd954bfaa4ef
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-02-02 14:56:05 +0600

    remove dulplicte form array using for

---
+++ removeDuplicatrewithfor.js
@@ -0,0 +1,13 @@
+let arr = [1,2,3,4,5,6,7,8,9,0,12,1,2,3,4,5,54,2,12223,43]
+
+let set = []
+
+for(let i = 0 ; i<arr.length ; i++){
+    let element = arr[i]
+    if(set.indexOf(element) == -1){
+        set.push(element)
+    }
+    
+
+}
+console.log(set);
